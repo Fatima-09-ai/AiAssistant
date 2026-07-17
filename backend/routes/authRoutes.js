925787ed2621171803fd22ca567283532bf7476e
Imama Fatima <fatimahimama09@gmail.com>
2026-07-17 15:58:27 +0500

    Add profile update and password change routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { register, login, logout, getMe } = require("../controllers/authController");
+const { register, login, logout, getMe, updateProfile, uploadAvatar, changePassword } = require("../controllers/authController");
 const { protect } = require("../middlewares/authMiddleware");
+const { avatarUpload } = require("../middlewares/uploadMiddleware");
 
 router.post("/register", register);
 router.post("/login", login);
 router.post("/logout", logout);
 router.get("/me", protect, getMe);
+router.patch("/me", protect, updateProfile);
+router.post("/me/avatar", protect, avatarUpload.single("avatar"), uploadAvatar);
+router.patch("/me/password", protect, changePassword);
 
 module.exports = router;
